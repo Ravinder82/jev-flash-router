e4993067770950304dd14f287a06f8177354bbd3
Ravinder Poonia <141393224+Ravinder82@users.noreply.github.com>
2026-09-20 07:49:05 +0530

    Update dotenv config to be quiet mode

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+// Redirect all stdout logs to stderr so MCP handshake doesn't break
+console.log = (...args: any[]) => process.stderr.write(args.join(" ") + "\n");
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import {
@@ -7,7 +9,7 @@ import {
 } from "@modelcontextprotocol/sdk/types.js";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ quiet: true });
 
 const server = new Server(
   {
